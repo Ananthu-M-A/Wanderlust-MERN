5fae57e8e48f5e2766d56786569eb3b8dd73760f
Ananthu M A <heartchaserananthu@gmail.com>
2024-03-18 20:31:32 +0530

    Booking Confirmation Mail with Pdf set up done.

--- backend/src/routes/home.ts
+++ backend/src/routes/home.ts
@@ -9,6 +9,9 @@ import User from '../models/user';
 import cloudinary from 'cloudinary';
 import multer from 'multer';
 import Booking from '../models/booking';
+import { transporter } from '../utils/NodeMailer';
+import PDFKit from 'pdfkit';
+import fs from 'fs';
 
 const stripe = new Stripe(process.env.STRIPE_API_KEY as string);
 const router = express.Router();
@@ -162,6 +165,79 @@ router.get('/:userId/order-result-page', verifyToken, async (req: Request, res: 
             bookingStatus: "active",
         });
         await newBooking.save();
+
+        const user = await User.findOne({ _id: req.params.userId });
+        const hotel = await Hotel.findOne({ _id: paymentData.hotelId });
+
+        const doc = new PDFKit();
+
+        const stream = fs.createWriteStream(`wanderlust_booking_id_${newBooking._id}.pdf`);
+        doc.pipe(stream);
+
+        doc.fontSize(12).text('Booking Receipt', { align: 'center' }).moveDown();
+        doc.fontSize(10).text(`Guest Name: ${user?.firstName} ${user?.lastName}`).moveDown();
+        doc.text(`Hotel Name: ${hotel?.name}`).moveDown();
+        doc.text(`Adult Count: ${newBooking.adultCount}`).moveDown();
+        doc.text(`Child Count: ${newBooking.childCount}`).moveDown();
+        doc.text(`Check-In: ${newBooking.checkIn}`).moveDown();
+        doc.text(`Check-Out: ${newBooking.checkOut}`).moveDown();
+        doc.text(`Room Detail: ${newBooking.roomDetail}`).moveDown();
+        doc.text(`Total Cost: ₹${newBooking.totalCost}`).moveDown();
+        doc.text(`Payment Status: Successfull`).moveDown();
+        doc.text(`Booking Date: ${newBooking.bookingDate}`).moveDown();
+
+        doc.end();
+
+        const emailContent = `
+        Dear ${user?.firstName} ${user?.lastName},
+
+We are thrilled to confirm your reservation at ${hotel?.name}! Your comfort and satisfaction are our top priorities, and we are excited to host you during your upcoming stay.
+
+Here are the details of your booking:
+
+Reservation ID: ${newBooking._id}
+Guest Name: ${user?.firstName} ${user?.lastName}
+Check-in Date: ${newBooking.checkIn}
+Check-out Date: ${newBooking.checkOut}
+Room Type: ${newBooking.roomDetail}
+Number of Guests: ${newBooking.adultCount + newBooking.childCount}
+
+Please review the information above to ensure everything is accurate. If there are any discrepancies or if you have any additional requests, please don't hesitate to contact us at http://localhost:5173/.
+
+As part of our commitment to providing exceptional service, we offer a range of amenities and facilities to enhance your stay. Whether you're looking to unwind by the pool, indulge in a delicious meal at our restaurant, or take advantage of our concierge services, we're here to make your experience memorable.
+
+Upon arrival, our friendly staff will be ready to assist you with the check-in process and answer any questions you may have. We want to ensure your stay is seamless and enjoyable from start to finish.
+
+Thank you for choosing ${hotel?.name}. We look forward to welcoming you and making your stay with us unforgettable.
+
+Warm regards,
+
+Administrator,
+${hotel?.name}
+${hotel?.city}, ${hotel?.country}`;
+
+        const mailOptions = {
+            from: process.env.NODE_MAILER_EMAIL,
+            to: user?.email,
+            subject: `Confirmation of Your Hotel Booking - [${newBooking._id}]`,
+            text: emailContent,
+            attachments: [
+                {
+                    filename: `wanderlust_booking_id_${newBooking._id}.pdf`,
+                    path: `wanderlust_booking_id_${newBooking._id}.pdf`
+                }
+            ]
+        };
+
+        transporter.sendMail(mailOptions, (error: any, info: any) => {
+            if (error) {
+                console.log(error);
+                const errorMsg = "Error sending booking details & pdf";
+                res.status(400).json({ message: errorMsg });
+            } else {
+                console.log('OTP email sent:', info.response);
+            }
+        });
         res.redirect(`http://localhost:5173/home/${newBooking._id}/order-result-page`)
     } catch (error) {
         console.log(error);
