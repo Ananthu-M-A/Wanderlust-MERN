5669968a43903988bed412db9b40d90f84dbcc81
Ananthu M A <heartchaserananthu@gmail.com>
2024-06-21 22:32:50 +0530

    Error fix - tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,11 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    container: {
-      padding: {
-        md: "10rem"
-      }
+    extend: {
+      backgroundImage: theme => ({
+        'img': "url('/hero-cover.jpg')",
+      }),
     },
   },
   plugins: [],
